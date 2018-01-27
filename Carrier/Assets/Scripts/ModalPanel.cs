21dcb847bdb44aab8026fc9c05a50252cdc2e7b0
Brandon Walsh <bmw9t@virginia.edu>
2018-01-26 21:01:03 -0500

    update script

--- Carrier/Assets/Scripts/ModalPanel.cs
+++ Carrier/Assets/Scripts/ModalPanel.cs
@@ -1,28 +1,55 @@
-// Yes/No/Cancel: A string, a Yes event, a No event and Cancel event
-	 public void Choice (string question, UnityAction yesEvent, UnityAction noEvent, UnityAction cancelEvent) {
-			 modalPanelObject.SetActive (true);
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
 
-			 yesButton.onClick.RemoveAllListeners();
-			 yesButton.onClick.AddListener (yesEvent);
-			 yesButton.onClick.AddListener (ClosePanel);
+//  This script will be updated in Part 2 of this 2 part series.
+public class ModalPanel : MonoBehaviour {
 
-			 noButton.onClick.RemoveAllListeners();
-			 noButton.onClick.AddListener (noEvent);
-			 noButton.onClick.AddListener (ClosePanel);
+    public Text question;
+    public Image iconImage;
+    public Button yesButton;
+    public Button noButton;
+    public Button maybeButton;
+    public GameObject modalPanelObject;
 
-			 cancelButton.onClick.RemoveAllListeners();
-			 cancelButton.onClick.AddListener (cancelEvent);
-			 cancelButton.onClick.AddListener (ClosePanel);
+    private static ModalPanel modalPanel;
 
-			 this.question.text = question;
+    public static ModalPanel Instance () {
+        if (!modalPanel) {
+            modalPanel = FindObjectOfType(typeof (ModalPanel)) as ModalPanel;
+            if (!modalPanel)
+                Debug.LogError ("There needs to be one active ModalPanel script on a GameObject in your scene.");
+        }
 
-			 this.iconImage.gameObject.SetActive (false);
-			 yesButton.gameObject.SetActive (true);
-			 noButton.gameObject.SetActive (true);
-			 cancelButton.gameObject.SetActive (true);
-	 }
+        return modalPanel;
+    }
 
-	 void ClosePanel () {
-			 modalPanelObject.SetActive (false);
-	 }
+    // Yes/No/maybe: A string, a Yes event, a No event and maybe event
+    public void Choice (string question, UnityAction yesEvent, UnityAction noEvent, UnityAction maybeEvent) {
+        modalPanelObject.SetActive (true);
+
+        yesButton.onClick.RemoveAllListeners();
+        yesButton.onClick.AddListener (yesEvent);
+        yesButton.onClick.AddListener (ClosePanel);
+
+        noButton.onClick.RemoveAllListeners();
+        noButton.onClick.AddListener (noEvent);
+        noButton.onClick.AddListener (ClosePanel);
+
+        maybeButton.onClick.RemoveAllListeners();
+        maybeButton.onClick.AddListener (maybeEvent);
+        maybeButton.onClick.AddListener (ClosePanel);
+
+        this.question.text = question;
+
+        this.iconImage.gameObject.SetActive (false);
+        yesButton.gameObject.SetActive (true);
+        noButton.gameObject.SetActive (true);
+        maybeButton.gameObject.SetActive (true);
+    }
+
+    void ClosePanel () {
+        modalPanelObject.SetActive (false);
+    }
 }
--- Carrier/Assets/Scripts/TestModalWindow.cs
+++ Carrier/Assets/Scripts/TestModalWindow.cs
@@ -10,7 +10,7 @@ public class TestModalWindow : MonoBehaviour {
 
     private UnityAction myYesAction;
     private UnityAction myNoAction;
-    private UnityAction myCancelAction;
+    private UnityAction myMaybeAction;
 
     void Awake () {
         modalPanel = ModalPanel.Instance ();
@@ -18,13 +18,13 @@ public class TestModalWindow : MonoBehaviour {
 
         myYesAction = new UnityAction (TestYesFunction);
         myNoAction = new UnityAction (TestNoFunction);
-        myCancelAction = new UnityAction (TestCancelFunction);
+        myMaybeAction = new UnityAction (TestMaybeFunction);
     }
 
     //  Send to the Modal Panel to set up the Buttons and Functions to call
-    public void TestYNC () {
-        modalPanel.Choice ("Do you want to spawn this sphere?", TestYesFunction, TestNoFunction, TestCancelFunction);
-//      modalPanel.Choice ("Would you like a poke in the eye?\nHow about with a sharp stick?", myYesAction, myNoAction, myCancelAction);
+    public void TestYMC () {
+        modalPanel.Choice ("Are you a bird?", TestYesFunction, TestNoFunction, TestMaybeFunction);
+//      modalPanel.Choice ("Would you like a poke in the eye?\nHow about with a sharp stick?", myYesAction, myNoAction, myMaybeAction);
     }
 
     //  These are wrapped into UnityActions
@@ -36,7 +36,7 @@ public class TestModalWindow : MonoBehaviour {
         displayManager.DisplayMessage ("No way, José!");
     }
 
-    void TestCancelFunction () {
-        displayManager.DisplayMessage ("I give up!");
+    void TestMaybeFunction () {
+        displayManager.DisplayMessage ("Maybe?!");
     }
 }
