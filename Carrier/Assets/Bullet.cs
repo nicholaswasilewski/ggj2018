3f4bada1ba4a4110a172a7706319a403d5e28cba
Andrew Davis <theandrewdavis@gmail.com>
2018-01-28 00:40:50 -0500

    bullet behavior

---
+++ Carrier/Assets/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	private Vector3 start;
+	private Vector3 vec;
+
+	void Start () {
+		start = transform.position;
+		vec = Random.onUnitSphere;
+		if (vec.y < 0)
+			vec.y = -vec.y;
+	}
+	
+	void Update () {
+		transform.position += vec * Time.deltaTime * 20f;
+		if ((transform.position - start).magnitude > 300f)
+			Destroy(gameObject);
+	}
+}
